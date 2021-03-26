aed7841a93ad286e210f067c3e87442c58d5db7e
yjkim0083 <yjkim0083@gmail.com>
2021-03-26 17:58:00 +0900

    pororo translation 기능 테스트

--- api/app.py
+++ api/app.py
@@ -13,8 +13,13 @@ mt = Pororo(task="translation", lang="multi")
 
 @app.get("/")
 def read_root():
-    result = mt("나는 모비젠이라는 회사에 다닌다.", src="ko", tgt="en")
-    return {"Hello": result}
+    #result = mt("나는 모비젠이라는 회사에 다닌다.", src="ko", tgt="en")
+    return {"Hello": "World"}
+
+
+@app.get("/translation/{text}")
+def read_root(text: str):
+    return {"ko": mt(text, src="en", tgt="ko"), "en": text}
 
 
 @app.get("/items/{item_id}")
