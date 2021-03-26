b801829a2cbd414fbf0810aaf77ebe27b083f028
yjkim0083 <yjkim0083@gmail.com>
2021-03-26 17:45:08 +0900

    pororo 적용

---
+++ api/app.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+# app.py
+import uvicorn
+from typing import Optional
+from fastapi import FastAPI
+
+from pororo import Pororo
+
+app = FastAPI()
+
+mt = Pororo(task="translation", lang="multi")
+
+
+@app.get("/")
+def read_root():
+    result = mt("나는 모비젠이라는 회사에 다닌다.", src="ko", tgt="en")
+    return {"Hello": result}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
